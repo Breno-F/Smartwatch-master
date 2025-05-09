e8ec9a76af271663d96ca38f44c3226a3f8bf40b
Breno <breno.cesar.gbs@gmail.com>
2025-05-09 18:24:50 -0300

    correção.0.50

--- src/main/java/com/example/AulaTeste/errors/PedidoSemEstoque.java
+++ src/main/java/com/example/AulaTeste/errors/PedidoSemEstoque.java
@@ -3,7 +3,7 @@ package com.example.AulaTeste.errors;
 public class PedidoSemEstoque extends RuntimeException {
   private String mensagem;
   public PedidoSemEstoque() {
-    this.mensagem = "O pedido está esgotado ";
+    this.mensagem = "O pedido está esgotado";
   }
 
   public String getMensagem() {
